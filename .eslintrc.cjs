8aa0553778da2546569c50be87fb543e848a61b8
Sinh Nguyen <nguyentrisinh0810@gmail.com>
2023-11-18 17:01:41 +0700

    feat: add husky lint-staged and more check on eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,25 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'prettier',
+    'eslint-config-prettier',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'lint-staged.config.js'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', '@typescript-eslint', 'simple-import-sort', 'eslint-plugin-prettier', 'unused-imports'],
   rules: {
-    'react-refresh/only-export-components': [
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/ban-types': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'prettier/prettier': ['error'],
+    'import/no-named-as-default': 0,
+    'simple-import-sort/imports': 'error',
+    'no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
       'warn',
-      { allowConstantExport: true },
+      { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
     ],
   },
-}
+};
